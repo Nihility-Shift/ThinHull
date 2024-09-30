58ff0b169016797df12248ff6acf6ede8c1a0a1c
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-09-30 13:53:41 -0700

    Added DocBuilder

---
+++ ThinHull/MyPluginInfo.cs
@@ -0,0 +1,17 @@
+#pragma warning disable CS1591
+namespace ThinHull
+{
+    //Auto-Generated File. Created by PreBuild.ps1
+    public class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
+        public const string PLUGIN_NAME = "ThinHull";
+        public const string USERS_PLUGIN_NAME = "Thin Hull";
+        public const string PLUGIN_VERSION = "0.0.2";
+        public const string PLUGIN_DESCRIPTION = "Makes hull breaches leak air from the ship. Host side.";
+        public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
+        public const string PLUGIN_AUTHORS = "18107";
+        public const string PLUGIN_THUNDERSTORE_ID = "VoidCrewModdingTeam/Thin_Hull";
+    }
+}
+#pragma warning restore CS1591
---
+++ ThinHull/ReleaseFiles/CHANGELOG.md
@@ -0,0 +1,8 @@
+## 0.0.2
+- Updated for Void Crew 0.27.0 (Update 5).
+
+## 0.0.1
+- Changed dependency from AtmosphereFix to VoidFixes (No change to the dll).
+
+## 0.0.0
+- Initial release.
