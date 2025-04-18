f9ed29f2e803abc8b6a6de8ca9dfefbcd70de554
Nagord <46509577+Nagord@users.noreply.github.com>
2025-04-18 16:31:35 -0700

    Updated documentation

--- ThinHull/MyPluginInfo.cs
+++ ThinHull/MyPluginInfo.cs
@@ -7,11 +7,11 @@ namespace ThinHull
         public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
         public const string PLUGIN_NAME = "ThinHull";
         public const string USERS_PLUGIN_NAME = "Thin Hull";
-        public const string PLUGIN_VERSION = "0.0.3";
+        public const string PLUGIN_VERSION = "0.0.4";
         public const string PLUGIN_DESCRIPTION = "Makes hull breaches leak air from the ship. Host side.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
         public const string PLUGIN_AUTHORS = "18107";
-        public const string PLUGIN_THUNDERSTORE_ID = "VoidCrewModdingTeam/Thin_Hull";
+        public const string PLUGIN_THUNDERSTORE_ID = "NihilityShift/Thin_Hull";
     }
 }
 #pragma warning restore CS1591
